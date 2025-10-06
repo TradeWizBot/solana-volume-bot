a0eba661db8ccfe23bb52783a8847620285cb935
KTSHACX <63502505+mine3krish@users.noreply.github.com>
2025-10-06 21:23:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const config = {
-    // rpc: 'https://mainnet.helius-rpc.com/?api-key=6ade3723-07c0-4936-a748-a73d1d36ffae',
-    // rpc: 'https://go.getblock.io/c9837f30112a402c9883fd95616b3785',
-    // rpc: 'https://solana-mainnet.core.chainstack.com/105286c92d78bd3cbcef3a30cb2f6d81',
-    // rpc: 'https://rpc-mainnet.solanatracker.io/?api_key=e56944a9-9557-4e90-96bc-3d9190fe2003',
-    rpc: 'https://api.mainnet-beta.solana.com',
+    rpc: 'https://solana-mainnet.core.chainstack.com/{endpoint_url}',
     tax_wallet: 'H4mu49XfHpvpMzuGWuKWGgDi6frS44TubxFbwmefy1ed',
     poolTax: 1, // 5%
     botTax: 0.001, // .01%
